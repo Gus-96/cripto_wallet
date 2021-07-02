ad576a59d567f1832cf22b211a84cef3449f1f14
Augusto da Silva <augusto.da.silva.com>
2021-07-02 01:47:21 -0300

    criando novo CRUD

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+    def soma
+    1 + 1
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_29_010537) do
+ActiveRecord::Schema.define(version: 2021_07_02_043808) do
 
   create_table "coins", force: :cascade do |t|
     t.string "url_image"
+    t.string "description"
+    t.string "acronym"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "mining_types", force: :cascade do |t|
     t.string "description"
     t.string "acronym"
     t.datetime "created_at", precision: 6, null: false
